714fda09ac8dfa153d05378020f26b9885150a6f
BUPTSSE-Zero <GGGZ-1101-28@Live.cn>
2017-07-10 23:30:23 +0800

    Fix crash bug due to not serializing class DeliveryInfo

--- app/src/main/java/happyyoung/trashnetwork/recycle/model/DeliveryInfo.java
+++ app/src/main/java/happyyoung/trashnetwork/recycle/model/DeliveryInfo.java
@@ -1,9 +1,11 @@
 package happyyoung.trashnetwork.recycle.model;
 
+import java.io.Serializable;
+
 /**
  * Created by shengyun-zhou <GGGZ-1101-28@Live.cn> on 2017-07-09
  */
-public class DeliveryInfo {
+public class DeliveryInfo implements Serializable {
     private String company;
     private String waybillId;
 
